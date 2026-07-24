9614a76a2ecdc255af4862847fbb25a9a3ed7c43
Shubhang Aarya <shubhang10dec@gmail.com>
2026-07-25 00:45:57 +0530

    Time: 0 ms (100%), Space: 11.8 MB (57.93%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=0;i<strs[0].size();i++){
             char ch =strs[0][i];
 
-            for(int j=0;j<strs.size();j++){
+            for(int j=1;j<strs.size();j++){
                 if (i >= strs[j].size() || strs[j][i] != ch) {
                     return strs[0].substr(0, i);
                 }
