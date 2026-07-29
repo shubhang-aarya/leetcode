e8549481b767de3a4407e768b2381d98414670a5
Shubhang Aarya <shubhang10dec@gmail.com>
2026-07-29 23:40:35 +0530

    Time: 0 ms (100%), Space: 7.8 MB (94.25%) - LeetHub

---
+++ 0400-nth-digit/0400-nth-digit.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findNthDigit(int n) {
+        long long digits = 1;   
+        long long count = 9;   
+        long long start = 1;    
+        
+        while (n > digits * count) {
+            n -= digits * count;
+            digits++;
+            count *= 10;
+            start *= 10;
+        }
+        long long number = start + (n - 1) / digits;
+        string s = to_string(number);
+        return s[(n - 1) % digits] - '0';
+    }
+};
