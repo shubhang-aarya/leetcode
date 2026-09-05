1528a24a0e79a54d3bd98ce8cb4916738dd33b03
Shubhang Aarya <shubhang10dec@gmail.com>
2026-09-05 23:27:33 +0530

    Time: 0 ms (100%), Space: 28.6 MB (48.78%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int singleSum = 0;
+        int doubleSum = 0;
+
+        for (int num : nums) {
+            if (num < 10)
+                singleSum += num;
+            else
+                doubleSum += num;
+        }
+
+        return singleSum != doubleSum;
+    }
+};
